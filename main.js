3c27b9b384d755604fef3bb6c724fa88f07e29ca
elbedawey11@gmail.com <elbedawey11@gmail.com>
2022-10-16 15:21:09 +0200

    add all files

---
+++ main.js
@@ -0,0 +1,25 @@
+//get the elements
+let advice = document.querySelector("#advice");
+let adviceNumber = document.querySelector("#adviceNumber");
+let adviceData = {};
+let randomButton = document.querySelector("button");
+// get the data from api
+function getDataFromApi() {
+    fetch("https://api.adviceslip.com/advice").then(
+        (result) => {
+            let data = result.json();
+            console.log(data);
+            return data;
+        }
+    ).then((data) => {
+        adviceData = data.slip;
+    }).then(
+        () => {
+            adviceNumber.innerHTML = `ADVICE #${adviceData.id}`;
+            advice.innerHTML = ` ${adviceData.advice} `;
+        }
+    )
+
+}
+randomButton.addEventListener("click", getDataFromApi);
+// getDataFromApi();
